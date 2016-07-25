a93d21c2225541756fa5142cddc75143ca7bebe5
Byungwook Lee <quddnr153@gmail.com>
2016-07-25 03:34:28 -0400

    quddnr commit

--- LocalSurePark/src/com/localsurepark/cmu/CurrentInfo.java
+++ LocalSurePark/src/com/localsurepark/cmu/CurrentInfo.java
@@ -2,8 +2,9 @@ package com.localsurepark.cmu;
 
 public class CurrentInfo {
 	public static int reservationID;
-	public static String stall1 = "0000000002";
-	public static String stall2 = "0000000003";
-	public static String stall3 = "0000000004";
-	public static String stall4 = "0000000005";
+	public static final String stall1 = "0000000002";
+	public static final String stall2 = "0000000003";
+	public static final String stall3 = "0000000004";
+	public static final String stall4 = "0000000005";
+	public static final String parkingLotID = "1";
 }
